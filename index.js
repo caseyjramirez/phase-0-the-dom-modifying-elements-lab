dcdfa661c2dab1c55226f5bf65bf3e561104899f
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-10 17:54:13 -0500

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const d = document;
+
+const main = d.querySelector('#main');
+main.remove();
+
+const newHeader = d.createElement('h1');
+newHeader.id = "victory"
+newHeader.textContent = 'Casey is the champion'
+
+d.append(newHeader);
